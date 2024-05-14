2a52df8405f3ff8695a97d8a7e2a30cb5e1a098f
phenix3443 <phenix3443@gmail.com>
2024-05-14 16:17:40 +0800

    feat: copy files from greenfield-cosmos-sdk for compiling bridge

---
+++ x/oracle/types/expected_keepers.go
@@ -0,0 +1,31 @@
+package types
+
+import (
+	"math/big"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking/types"
+)
+
+type StakingKeeper interface {
+	GetLastValidators(ctx sdk.Context) (validators []types.Validator)
+	GetHistoricalInfo(ctx sdk.Context, height int64) (types.HistoricalInfo, bool)
+	BondDenom(ctx sdk.Context) (res string)
+}
+
+type CrossChainKeeper interface {
+	CreateRawIBCPackageWithFee(ctx sdk.Context, destChainId sdk.ChainID, channelID sdk.ChannelID,
+		packageType sdk.CrossChainPackageType, packageLoad []byte, relayerFee, ackRelayerFee *big.Int,
+	) (uint64, error)
+	GetCrossChainApp(channelID sdk.ChannelID) sdk.CrossChainApplication
+	GetSrcChainID() sdk.ChainID
+	IsDestChainSupported(chainID sdk.ChainID) bool
+	GetReceiveSequence(ctx sdk.Context, chainId sdk.ChainID, channelID sdk.ChannelID) uint64
+	IncrReceiveSequence(ctx sdk.Context, chainId sdk.ChainID, channelID sdk.ChannelID)
+	GetDestBscChainID() sdk.ChainID
+	GetDestOpChainID() sdk.ChainID
+}
+
+type BankKeeper interface {
+	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
+}
